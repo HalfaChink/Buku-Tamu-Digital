27594a19b8cc9155b1edcf82c275b39aa6f78d2a
Rmt_47 <eighteenofmarch07@gmail.com>
2024-10-06 17:11:59 +0700

    edit tampilan sidebar & topbar + nambahan isi tiap halaman admin (cara make warna custom kumaha njir?)

--- resources/views/adminpanel/charts.blade.php
+++ resources/views/adminpanel/charts.blade.php
@@ -18,7 +18,7 @@
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="{{ asset('admin/css/sb-admin-2.min.css') }}" rel="stylesheet">
+    <link href="{{ url('admin/css/sb-admin-2.min.css') }}" rel="stylesheet">
 
 </head>
 
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('./adminpanel/index');
+    return view('./adminpanel/charts');
 });
 
 Route::get('/charts', function () {
     return view('./adminpanel/charts');
+});
+
+Route::get('/edit', function () {
+    return view('./adminpanel/edit');
+});
+
+Route::get('/historytable', function () {
+    return view('./adminpanel/historytable');
+});
+
+Route::get('/usertable', function () {
+    return view('./adminpanel/usertable');
 });
