f40fa2b4a35f25eb857ab3b6f2925ed9a1b19c84
Rmt_47 <eighteenofmarch07@gmail.com>
2024-10-11 14:13:19 +0700

    edit dahbooard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,13 +5,88 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('./sass/dashboard.scss') }}">
+    <style>
+        #header {
+           
+        }
+    </style>
 </head>
+
 
 <body>
     <!-- Header -->
-    <div>
-        <a href="/register">masuk</a>
-    </div>
+    <section id="header">
+
+        <!-- Navbar  -->
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Buku Tamu</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span>Menu</span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Login Admin</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Kunjungan Hari Ini</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- End of Navbar -->
+
+        <!-- Hero -->
+        <div class="mx-auto">
+
+            <!-- Carousell -->
+            <div id="carouselExampleIndicators" class="carousel slide w-75 mx-auto">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0"
+                        class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+                        aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+                        aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="{{ asset('admin/img/logo_artristik.png') }}" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="{{ asset('admin/img/logo_artristik.png') }}" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="{{ asset('admin/img/logo_artristik.png') }}" class="d-block w-100" alt="...">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+                    data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+                    data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+            <!-- End of Carousell -->
+
+            <!-- Register -->
+            <a class="btn mx-auto" href="#" role="button" style="background-color: #3b1f4b;">Link</a>
+
+
+        </div>
+
+    </section>
     <!-- End of Header -->
 
     <!-- Today's History Section -->
@@ -33,8 +108,13 @@
     <!-- End of Comment Section -->
 
     <!-- Bootstrap core JavaScript-->
-    <script src="{{ asset('admin/vendor/jquery/jquery.min.js') }}"></script>
-    <script src="{{ asset('admin/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+        integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
+        crossorigin="anonymous"></script>
+
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,6 @@ Route::get('/charts', function () {
     return view('./adminpanel/charts');
 });
 
-<<<<<<< HEAD
-=======
-// <<<<<<< HEAD
->>>>>>> 9dd77ea129763191aaa19edb55c7b27861f39804
-
-// >>>>>>> refs/remotes/origin/local
 Route::get('/edit', function () {
     return view('./adminpanel/edit');
 });
@@ -27,8 +21,6 @@ Route::get('/historytable', function () {
 
 Route::get('/usertable', function () {
     return view('./adminpanel/usertable');
-// <<<<<<< HEAD
-// >>>>>>> af3bf4e3e906495339cf0e1de47735504e67c97c
 });
 
 // register
@@ -39,13 +31,8 @@ Route::get('/register', function () {
 Route::get('/register2', function () {
     return view('register2');
 
-// >>>>>>> refs/remotes/origin/local
 });
 
 Route::get('/loginadmin', function () {
     return view('./adminpanel/loginadmin');
-});
-
-Route::get('/register', function () {
-    return view('/register');
 });
