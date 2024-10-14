d51babd4c22816814579b1127cb8b93455043b9e
Rmt_47 <eighteenofmarch07@gmail.com>
2024-10-14 15:11:31 +0700

    perubahan kecil di dashboard (belum beres)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,9 +9,7 @@
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('./sass/dashboard.scss') }}">
     <style>
-        #header {
-           
-        }
+        #header {}
     </style>
 </head>
 
@@ -21,7 +19,7 @@
     <section id="header">
 
         <!-- Navbar  -->
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <nav class="navbar navbar-expand-lg">
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">Buku Tamu</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
@@ -47,7 +45,7 @@
         <div class="mx-auto">
 
             <!-- Carousell -->
-            <div id="carouselExampleIndicators" class="carousel slide w-75 mx-auto">
+            <div id="carouselExampleIndicators" class="carousel slide w-75 mx-auto ">
                 <div class="carousel-indicators">
                     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0"
                         class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -56,25 +54,25 @@
                     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
                         aria-label="Slide 3"></button>
                 </div>
-                <div class="carousel-inner shadow-lg p-0 m-4 bg-body rounded">
+                <div class="carousel-inner shadow-lg p-0 m-4 rounded" style="background-color: #3b1f4b;">
                     <div class="carousel-item active">
-                        <img src="{{ asset('admin/img/logo.png') }}" class="d-block mx-auto" alt="...">
+                        <img src="{{ asset('admin/img/logotest.png') }}" class="d-block mx-auto w-75 p-5" alt="...">
                     </div>
                     <div class="carousel-item">
-                        <img src="{{ asset('admin/img/logo.png') }}" class="d-block mx-auto" alt="...">
+                        <img src="{{ asset('admin/img/logotest.png') }}" class="d-block mx-auto w-75 p-5" alt="...">
                     </div>
                     <div class="carousel-item">
-                        <img src="{{ asset('admin/img/logo.png') }}" class="d-block mx-auto" alt="...">
+                        <img src="{{ asset('admin/img/logotest.png') }}" class="d-block mx-auto w-75 p-5" alt="...">
                     </div>
                 </div>
                 <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
                     data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon bg-black" aria-hidden="true"></span>
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span class="visually-hidden">Previous</span>
                 </button>
                 <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
                     data-bs-slide="next">
-                    <span class="carousel-control-next-icon bg-black" aria-hidden="true"></span>
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
                     <span class="visually-hidden">Next</span>
                 </button>
             </div>
@@ -82,25 +80,53 @@
 
         </div>
 
-          <!-- Register -->
-          <div class="d-flex align-middle m-5">
-            <a class="btn text-light text-uppercase mx-auto" href="{{ route('register') }}" role="button" style="background-color: #3b1f4b;">berkunjung</a>
-          </div>
+        <!-- Register -->
+        <div class="d-flex align-middle m-5">
+            <a class="btn text-light text-uppercase mx-auto" href="{{ route('register') }}" role="button"
+                style="background-color: #3b1f4b;">berkunjung</a>
+        </div>
 
     </section>
     <!-- End of Header -->
 
-    <!-- Today's History Section -->
-    <div>
+    <section id="content">
 
-    </div>
-    <!-- End of Today's History Section -->
+        <div id="hari-ini" class="p-5" style="background-color: #3b1f4b; color: white;">
+            <div class="d-flex justify-content-between">
+                <h2 class=>Kunjungan Hari Ini</h2>
+                <h2 class=>22 September 2077</h2>
+            </div>
+            <table class="mx-auto bg-white rounded-3 m-4" style="color: #3b1f4b;">
+                <thead>
+                    <tr>
+                        <th class="col-1 p-5">No</th>
+                        <th class="col-4">Nama</th>
+                        <th class="col-7">Tujuan Kunjungan</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th class="p-5"></th>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
 
-    <!-- Last History Section -->
-    <div>
+        <div id="hari-ini" class="p-5">
+            <h2>Pengunjung Sebelumnya</h2>
+            <table>
+                <tr>
+                    <th class="p-5">No</th>
+                    <th>Tanggal</th>
+                    <th>Nama</th>
+                    <th>Tujuan Kunjungan</th>
+                </tr>
+            </table>
+        </div>
 
-    </div>
-    <!-- End of Last History Section -->
+    </section>
 
     <!--  Footer Section -->
     <div class="d-flex justify-content-center align-items-center p-5" style="background-color: rgb(59, 32, 75)">
