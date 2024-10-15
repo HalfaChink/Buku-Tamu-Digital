ddfbf87a85f680627fab5472bc07cf1d61c55d81
Rmt_47 <eighteenofmarch07@gmail.com>
2024-10-15 18:45:07 +0700

    nu kamari ck p fazal

--- resources/views/adminpanel/charts.blade.php
+++ resources/views/adminpanel/charts.blade.php
@@ -1,19 +1,19 @@
+@extends('layout/app')
+
+@section('title')
+Diagram
+@endsection
+
+@section('content')
+
 <!-- Page Wrapper -->
 <div id="wrapper">
-
-    <!-- Sidebar -->
-    <x-sidebar></x-sidebar>
-    <!-- End of Sidebar -->
 
     <!-- Content Wrapper -->
     <div id="content-wrapper" class="d-flex flex-column">
 
         <!-- Main Content -->
         <div id="content">
-
-            <!-- Topbar -->
-            <x-topbar></x-topbar>
-            <!-- End of Topbar -->
 
             <!-- Begin Page Content -->
             <div class="container-fluid">
@@ -88,18 +88,9 @@
         </div>
         <!-- End of Main Content -->
 
-        <!-- Footer -->
-        <footer class="sticky-footer bg-white">
-            <div class="container my-auto">
-                <div class="copyright text-center my-auto">
-                    <span>Copyright &copy; Your Website 2020</span>
-                </div>
-            </div>
-        </footer>
-        <!-- End of Footer -->
-
     </div>
     <!-- End of Content Wrapper -->
 
 </div>
 <!-- End of Page Wrapper -->
+@endsection
