4132891953b237e55b1fd41c90c57961ddc9a957
Rmt_47 <eighteenofmarch07@gmail.com>
2024-10-16 12:05:57 +0700

    edit tampilan + koneksi db

--- resources/views/adminpanel/charts.blade.php
+++ resources/views/adminpanel/charts.blade.php
@@ -19,66 +19,26 @@ Diagram
             <div class="container-fluid bg-white">
 
                 <!-- Page Heading -->
-                <h1 class="h3 mb-2 text-gray-800">Charts</h1>
-                <p class="mb-4">Chart.js is a third party plugin that is used to generate the charts in this theme.
-                    The charts below have been customized - for further customization options, please visit the <a
-                        target="_blank" href="https://www.chartjs.org/docs/latest/">official Chart.js
-                        documentation</a>.</p>
+                <h1 class="h3 mb-2 text-gray-800">Grafik Kunjungan</h1>
 
                 <!-- Content Row -->
-                <div class="row">
+                <div>
 
-                    <div class="col-xl-8 col-lg-7">
+                    <div>
 
-                        <!-- Area Chart -->
-                        <div class="card shadow mb-4">
-                            <div class="card-header py-3">
-                                <h6 class="m-0 font-weight-bold text-primary">Area Chart</h6>
-                            </div>
-                            <div class="card-body">
-                                <div class="chart-area">
-                                    <canvas id="myAreaChart"></canvas>
-                                </div>
-                                <hr>
-                                Styling for the area chart can be found in the
-                                <code>/js/demo/chart-area-demo.js</code> file.
-                            </div>
-                        </div>
 
                         <!-- Bar Chart -->
                         <div class="card shadow mb-4">
-                            <div class="card-header py-3">
+                            <div class="card-header">
                                 <h6 class="m-0 font-weight-bold text-primary">Bar Chart</h6>
                             </div>
                             <div class="card-body">
                                 <div class="chart-bar">
                                     <canvas id="myBarChart"></canvas>
                                 </div>
-                                <hr>
-                                Styling for the bar chart can be found in the
-                                <code>/js/demo/chart-bar-demo.js</code> file.
                             </div>
                         </div>
 
-                    </div>
-
-                    <!-- Donut Chart -->
-                    <div class="col-xl-4 col-lg-5">
-                        <div class="card shadow mb-4">
-                            <!-- Card Header - Dropdown -->
-                            <div class="card-header py-3">
-                                <h6 class="m-0 font-weight-bold text-primary">Donut Chart</h6>
-                            </div>
-                            <!-- Card Body -->
-                            <div class="card-body">
-                                <div class="chart-pie pt-4">
-                                    <canvas id="myPieChart"></canvas>
-                                </div>
-                                <hr>
-                                Styling for the donut chart can be found in the
-                                <code>/js/demo/chart-pie-demo.js</code> file.
-                            </div>
-                        </div>
                     </div>
                 </div>
 
@@ -93,4 +53,9 @@ Diagram
 
 </div>
 <!-- End of Page Wrapper -->
+
+<script src="{{ asset('admin/js/demo/chart-area-demo.js') }}"></script>
+<script src="{{ asset('admin/js/demo/chart-pie-demo.js') }}"></script>
+<script src="{{ asset('admin/js/demo/chart-bar-demo.js') }}"></script>
+
 @endsection
