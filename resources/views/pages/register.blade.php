2f5579e9b57556f5d242ba3cabb4155e408096df
FauziRamadhan11 <ramadhanfauzii932@gmail.com>
2024-10-27 19:36:55 +0700

    edit responsive

--- resources/views/pages/register.blade.php
+++ resources/views/pages/register.blade.php
@@ -27,7 +27,7 @@
     <div class="px-5 py-4 px-md-5 text-lg-start" style="background-color: rgb(59, 32, 75); height: 100vh;">
       <div class="container">
         <div class="row gx-lg-5 align-items-center">
-          <div class="col-lg-6 d-lg-block d-md-block d-none mb-5 mb-lg-0 text-center">
+          <div class="col-lg-6 d-lg-block d-none mb-5 mb-lg-0 text-center">
             <img src="{{ asset('admin/img/logotest.png') }}" alt="PT.artristik Studio Bandung" width="400">
           </div>
 
