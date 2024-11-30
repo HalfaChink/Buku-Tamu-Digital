5540a02ffce2aef7e7c36d334acad60e07b3139d
FauziRamadhan11 <ramadhanfauzii932@gmail.com>
2024-11-30 09:24:42 +0700

    edit responsive part

--- resources/views/adminpanel/edit.blade.php
+++ resources/views/adminpanel/edit.blade.php
@@ -6,6 +6,25 @@ Edit
 
 @section('content')
 
+<style>
+         button{
+             width: 45%;
+         }
+         .btn-danger{
+            float: right;
+            margin-top: -42px;
+        }
+     @media screen and (max-width: 576px){
+        button{
+        width: 100%;
+        }
+        .btn-danger{
+            float: none;
+            margin-top: 5px;
+        }
+     }
+</style>
+
 <div class="container">
     <h2>Edit Carousel</h2>
 
@@ -13,10 +32,10 @@ Edit
     <div class="row mb-4">
         @foreach($carouselImages as $carousel)
         <div class="col-md-4">
-            <img src="{{ asset('admin/img/' . $carousel->image) }}" class="img-thumbnail mb-2" alt="carousel image">
+            <img src="{{ asset('admin/img/' . $carousel->image) }}" class="img-thumbnail my-3" alt="carousel image">
             <form action="{{ route('carousel.update', $carousel->id) }}" method="POST" enctype="multipart/form-data">
                 @csrf
-                <input type="file" name="image" class="form-control mb-2">
+                <input type="file" name="image" class="form-control mb-2 p-1">
                 <button type="submit" class="btn btn-primary">Update Image</button>
             </form>
             <form action="{{ route('carousel.destroy', $carousel->id) }}" method="POST" class="mt-1">
@@ -28,10 +47,10 @@ Edit
         @endforeach
     </div>
 
-    <h4>Tambah Image Baru</h4>
+    <h4 class="mt-5 mb-2">Tambah Image Baru</h4>
     <form action="{{ route('carousel.store') }}" method="POST" enctype="multipart/form-data">
         @csrf
-        <input type="file" name="image" class="form-control mb-2">
+        <input type="file" name="image" class="form-control mb-2 p-1">
         <button type="submit" class="btn btn-success">Tambahkan Image</button>
     </form>
 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
 <body>
     <div id="wrapper">
         <!-- Sidebar -->
-        <ul class="navbar-nav bg-danger sidebar sidebar-dark accordion" id="accordionSidebar">
+        <ul class="navbar-nav sidebar sidebar-dark accordion" id="accordionSidebar" style="background-color: rgb(59, 32, 75);">
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
                 <div class="sidebar-brand-text mx-3">Admin</div>
             </a>
